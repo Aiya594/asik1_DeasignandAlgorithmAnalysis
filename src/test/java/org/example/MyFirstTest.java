4ee4f93a7dbd7b9130721e1c2cbfae8b3b9e7de6
aiya <aichkaxxx2007@gmail.com>
2025-09-20 14:18:05 +0500

    init: maven, junit5, readme

---
+++ src/test/java/org/example/MyFirstTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyFirstTest {
+
+    @Test
+    void additionShouldWork() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
+
+
+
